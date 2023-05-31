b9bbc5d83aa650b5704339dabf43f2a3b6db1db0
zahav740 <zahav740@gmail.com>
2023-05-31 08:28:18 +0300

    888

--- app/src/main/java/com/alexey/homeec/CsvHelper.java
+++ app/src/main/java/com/alexey/homeec/CsvHelper.java
@@ -56,3 +56,4 @@ public class CsvHelper {
         return transactions;
     }
 }
+
---
+++ app/src/main/java/com/alexey/homeec/Transction1.java
@@ -0,0 +1,39 @@
+package com.alexey.homeec;
+
+public class Transction1 {
+
+    /*
+    package com.alexey.homeec;
+
+public class Transaction {
+    private String date;
+    private double income;
+    private String expenseName;
+    private double expense;
+
+    public Transaction(String date, double income, String expenseName, double expense) {
+        this.date = date;
+        this.income = income;
+        this.expenseName = expenseName;
+        this.expense = expense;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public String getExpenseName() {
+        return expenseName;
+    }
+
+    public double getExpense() {
+        return expense;
+    }
+}
+
+     */
+}
